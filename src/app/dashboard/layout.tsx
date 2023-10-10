b07795de52b8a3a2083f2f5de01d542a9d853e00
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-10-10 14:21:17 -0300

    Subindo estrutura inicial para menu

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,65 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme } from "antd";
+import style from "./layout.module.scss";
 
 export type DataLayoutDashboard = {
   children: React.ReactNode;
 };
 
 export default function LayoutDashboard({ children }: DataLayoutDashboard) {
-  return <main>{children}</main>;
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout id={style.LayoutDashboard}>
+      <Layout.Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Layout.Sider>
+      <Layout>
+        <Layout.Header className={style.header}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Layout.Header>
+        <Layout.Content className={style.content}>{children}</Layout.Content>
+      </Layout>
+    </Layout>
+  );
 }
