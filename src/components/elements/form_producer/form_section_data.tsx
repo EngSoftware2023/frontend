d8252b7bb19623740943810321f81f98fef0e844
LucasVaz <lucas.vaz@appmoove.com.br>
2023-11-15 13:47:02 -0300

    fix: Atualizando a forma com que crio o objeto com os dados do produtor assim pegando os parametros da url.

--- src/components/elements/form_producer/form_section_data.tsx
+++ src/components/elements/form_producer/form_section_data.tsx
@@ -1,39 +1,62 @@
+import { useState, useEffect } from 'react';
 import { IUsers } from "@/types/types";
 import FormProducer from "./form_producer";
 import style from "./form-register-producer.module.scss";
 import StructContainer from "@/components/structs/container/container";
-import Api from "@/service/api/api";
+import { useSearchParams } from 'next/navigation';
 
-export interface IProps {
-    userId: string
-}
 
-const getPosts = async (id: string): Promise<IUsers[]> => {
-    const posts = await Api.public.getProducers();
-    return posts;
-};
-export default async function FormSectionData({ userId }: IProps) {
-    const users: IUsers[] = await getPosts('1233');
-    let userSelected: IUsers = {
+
+export default function FormSectionData() {
+    const searchParams = useSearchParams()
+
+    const name = searchParams.get('name');
+    const phone = searchParams.get('phone');
+    const email = searchParams.get('email');
+    const cpf = searchParams.get('cpf');
+    const address = searchParams.get('address');
+    const password = searchParams.get('password');
+
+
+
+    const [user, setUser] = useState<IUsers>({
         name: '',
         phone: '',
         address: '',
         email: '',
         cpf: '',
         password: '',
-    };
-     users.map((user) => {
-        console.log(user,userId)
-        if (user.cpf == userId) {
-            userSelected = user;
+    });
+
+    useEffect(() => {
+        console.log('Name:', name);
+        console.log('Phone:', phone);
+        console.log('Email:', email);
+        console.log('CPF:', cpf);
+        console.log('Address:', address);
+        console.log('Password:', password);
+        if (name != null && phone != null && address != null && email != null && cpf != null && password != null) {
+            setUser({
+                name: name,
+                phone: phone,
+                address: address,
+                email: email,
+                cpf: cpf,
+                password: password,
+            })
         }
-    })
+        console.log(user)
+    },[])
+
 
     return (
         <StructContainer className={style.containerForms}>
             <h2>Atualização de cadastro</h2>
             <hr className={style.divisor} />
-            <FormProducer user={userSelected} />
+           {
+            user.name!=''? <FormProducer user={user} />:<></>
+           }
+
         </StructContainer>
     )
 }
