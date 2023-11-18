c1f072a56b4b194d8d9d7edd4f257f4f1ffcc861
LucasVaz <lucas.vaz@appmoove.com.br>
2023-11-18 09:51:42 -0300

    feat: adicionando caminhos de acesso a api export para acessar as funções de alterar e deletar uma produção.

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -1,11 +1,13 @@
 import { postProducer, getProducers, updateProducers, getProducer } from "./endpoints/producer";
+import { getProductions, deteleProduction } from "./endpoints/production";
+
 
 export const API_BASE = "http://hendrickscheifer.pythonanywhere.com/api";
 export const API_BASE_PUT = "http://hendrickscheifer.pythonanywhere.com";
 
 
 const Api = {
-  public: { postProducer, getProducers, updateProducers, getProducer },
+  public: { postProducer, getProducers, updateProducers, getProducer, getProductions, deteleProduction },
 };
 
 export default Api;
