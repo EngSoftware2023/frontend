6b8d1ceb8b4c88ef524771b0ac66ebb5b0e1b3be
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-11-23 17:38:12 -0300

    [17.12] Adicionado opção de logout

--- src/app/dashboard/produtor/layout.tsx
+++ src/app/dashboard/produtor/layout.tsx
@@ -2,7 +2,6 @@ import StructMenu from "@/components/structs/menu/menu";
 import React from "react";
 
 import {
-  FormOutlined,
   FundOutlined,
   OrderedListOutlined,
   HomeOutlined,
@@ -13,35 +12,33 @@ export type DataLayoutProducer = {
   children: React.ReactNode;
 };
 
+const options = [
+  {
+    icon: <HomeOutlined />,
+    name: "Pagina inicial",
+    link: "/dashboard/produtor",
+  },
+  {
+    icon: <UserOutlined />,
+    name: "Ver Perfil",
+    link: "/dashboard/produtor/visualizar",
+  },
+  {
+    icon: <FundOutlined />,
+    name: "Produção",
+    link: "/dashboard/produtor/producao",
+  },
+  {
+    icon: <OrderedListOutlined />,
+    name: "Ver Relato de produção",
+    link: "/dashboard/produtor/verRelato",
+  },
+];
+
 export default function LayoutProducer({ children }: DataLayoutProducer) {
   return (
     <main>
-      <StructMenu
-        options={[
-          {
-            icon: <HomeOutlined />,
-            name: "Pagina inicial",
-            link: "/dashboard/produtor",
-          },
-          {
-            icon: <UserOutlined />,
-            name: "Ver Perfil",
-            link: "/dashboard/produtor/visualizar",
-          },
-          {
-            icon: <FundOutlined />,
-            name: "Produção",
-            link: "/dashboard/produtor/producao",
-          },
-          {
-            icon: <OrderedListOutlined />,
-            name: "Ver Relato de produção",
-            link: "/dashboard/produtor/verRelato",
-          },
-        ]}
-      >
-        {children}
-      </StructMenu>
+      <StructMenu options={options}>{children}</StructMenu>
     </main>
   );
 }
