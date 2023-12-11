6886e2fd5d130c5979383eec01a811e27bdb0a9c
LucasVaz <lucas.vaz@appmoove.com.br>
2023-12-11 00:14:39 -0300

    6.1 feat: Criando botões para verificar se pedido pode ser atendido.

--- src/components/structs/order/order.tsx
+++ src/components/structs/order/order.tsx
@@ -4,17 +4,20 @@ import SectionListOrder from "@/components/sections/list-orders/list-orders";
 import Api from "@/service/api/api";
 import { DataGetOrders } from "@/service/api/endpoints/order";
 import Auth from "@/service/auth/auth";
+import { IProduct } from "@/types/types";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export type DataStructOrder = {
   products: Array<string>;
+  productsMatch: IProduct[];
   initialOrders: DataGetOrders;
 };
 
 export default function StructOrder({
   products,
   initialOrders,
+  productsMatch
 }: DataStructOrder) {
   const router = useRouter();
   const [orders, setOrders] = useState<DataGetOrders>(initialOrders);
@@ -27,7 +30,7 @@ export default function StructOrder({
           setOrders(await Api.private.getOrders(auth));
         }}
       />
-      <SectionListOrder orders={orders} products={products}/>
+      <SectionListOrder orders={orders} products={products} productsMatch={productsMatch} />
     </>
   );
 }
